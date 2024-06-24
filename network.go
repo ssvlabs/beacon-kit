79086eb0b9431a32126edb97ced527412d1f168f
moshe-blox <moshe@blox.io>
2024-06-24 16:02:48 +0300

    feat: add Holesky network

--- network.go
+++ network.go
@@ -58,6 +58,40 @@ var (
 		DomainApplicationBuilder:          [4]byte{0, 0, 0, 1},
 	}
 
+	Holesky = &Spec{
+		Network:                              "holesky",
+		GenesisTime:                          time.Unix(1695902400, 0),
+		GenesisSlot:                          0,
+		GenesisForkVersion:                   phase0.Version{0x01, 0x01, 0x70, 0x00},
+		FarFutureEpoch:                       phase0.Epoch(math.MaxUint64),
+		SlotsPerEpoch:                        32,
+		SecondsPerSlot:                       12,
+		MaxCommitteesPerSlot:                 64,
+		TargetCommitteeSize:                  128,
+		TargetAggregatorsPerCommittee:        16,
+		AttestationSubnetCount:               64,
+		AttestationPropagationSlotRange:      32,
+		SyncCommitteeSize:                    512,
+		SyncCommitteeSubnetCount:             4,
+		TargetAggregatorsPerSyncSubcommittee: 16,
+		EpochsPerSyncCommitteePeriod:         256,
+		AltairForkEpoch:                      0,
+		BellatrixForkEpoch:                   0,
+
+		DomainBeaconProposer:              [4]byte{0, 0, 0, 0},
+		DomainBeaconAttester:              [4]byte{1, 0, 0, 0},
+		DomainRandao:                      [4]byte{2, 0, 0, 0},
+		DomainDeposit:                     [4]byte{3, 0, 0, 0},
+		DomainVoluntaryExit:               [4]byte{4, 0, 0, 0},
+		DomainSelectionProof:              [4]byte{5, 0, 0, 0},
+		DomainAggregateAndProof:           [4]byte{6, 0, 0, 0},
+		DomainSyncCommittee:               [4]byte{7, 0, 0, 0},
+		DomainSyncCommitteeSelectionProof: [4]byte{8, 0, 0, 0},
+		DomainContributionAndProof:        [4]byte{9, 0, 0, 0},
+		DomainApplicationMask:             [4]byte{0, 0, 0, 1},
+		DomainApplicationBuilder:          [4]byte{0, 0, 0, 1},
+	}
+
 	Prater = &Spec{
 		Network:                              "prater",
 		GenesisTime:                          time.Unix(1616508000, 0),
